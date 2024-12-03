30134a93fc3a21fe3883c82000df76de891055e2
Ramon Valeriano <rrvaleriano@gmail.com>
2024-12-02 21:01:50 -0300

    Criando meu primeiro projeto node com servidor para backend

---
+++ serve.js
@@ -0,0 +1,17 @@
+import http from "http";
+
+const PORT = 3000;
+const rotas = {
+  "/": "Curso NodeJs",
+  "/livros": "Entrei na rota Livros",
+  "/autores": "Entrei na rota Autores"
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+  console.log("Servidor Escutando!");
+});
